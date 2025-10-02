bca4e83b5c90e2a10ecfaf727f6a05486f7d5188
Szovát Ézsely <szovat.ezsely@adroitgroup.io>
2025-10-02 11:37:29 +0200

    FEATURE-1: Add initial files for project.

---
+++ src/main/java/com/eszsz/concept_tester/ConceptTesterApplication.java
@@ -0,0 +1,13 @@
+package com.eszsz.concept_tester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceptTesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eszsz/concept_tester/ConceptTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eszsz.concept_tester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConceptTesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
